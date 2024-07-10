7a2fbbfe1efc0c9c24b98df9e245f986ee67d66a
SimGo123 <43723284+SimGo123@users.noreply.github.com>
2024-07-10 11:04:28 +0200

    Implement basic wiki search, style searchbar and results

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -15,20 +15,20 @@ except Exception as e:
     print(f"Error importing wiki2: {e}")
     sys.exit(1)
 
-cgitb.enable()  # Enable CGI error reporting
+cgitb.enable()
 
 iso_list = []
 
 form = cgi.FieldStorage()
-input_text = form.getvalue("inputText")
+input_mountain = form.getvalue("mountain")
 
-if input_text:
+if input_mountain:
     # iso_list = [{'name': 'Piz Morteratsch', 'height': 3751.0, 'coords': [46.4025, 9.90166667], 'isolation': 1.6, 'next_high': 'Piz Bernina'}, {'name': 'Piz Bernina', 'height': 4048.0, 'coords': [46.38222222, 9.90805556], 'isolation': 138.0, 'next_high': 'Finsteraarhorn'}, {'name': 'Finsteraarhorn', 'height': 4274.0, 'coords': [46.53722222, 8.12611111], 'isolation': 51.65, 'next_high': 'Nadelhorn'}, 
 	# {'name': 'Nadelhorn', 'height': 4327.0, 'coords': [46.10888889, 7.86388889], 'isolation': 1.7, 'next_high': 'Dom [Berg]'}, {'name': 'Dom [Berg]', 'height': 4546.0, 'coords': [46.094, 7.85883333], 'isolation': 16.6, 'next_high': 'Nordend [Berg]'}, {'name': 'Nordend [Berg]', 'height': 4608.0, 'coords': [45.94166667, 7.87], 'isolation': 0.575, 'next_high': 'Grenzgipfel'}, {'name': 'Grenzgipfel', 'height': 4617.0, 'coords': [45.936719, 7.868561], 'isolation': 0.1, 'next_high': 'Dunantspitze'}, 
 	# {'name': 'Dunantspitze', 'height': 4631.0, 'coords': [45.93694444, 7.86777778], 'isolation': 0.1, 'next_high': 'Dufourspitze'}, {'name': 'Dufourspitze', 'height': 4634.0, 'coords': [45.9369, 7.86675], 'isolation': 78.2, 'next_high': 'Mont Blanc de Courmayeur'}, {'name': 'Mont Blanc de Courmayeur', 'height': 4748.0, 'coords': [45.828695, 6.86843], 'isolation': 0.6, 'next_high': 'Mont Blanc'}, {'name': 'Mont Blanc', 'height': 4805.59, 'coords': [45.832544, 6.864325], 'isolation': 2812.0, 'next_high': 'Kjukjurtlju'}, {'name': 'Kjukjurtlju', 'height': 4978.0, 'coords': [43.34138889, 42.40611111], 'isolation': 2.7, 'next_high': 'Elbrus'}, {'name': 'Elbrus', 'height': 5642.0, 'coords': [43.35254, 42.437875], 'isolation': 2472.7, 'next_high': 'Pik Agasis'}, 
     # {'name': 'Pik Agasis', 'height': 5877.0, 'coords': [39.014128, 71.504167], 'isolation': 29.45, 'next_high': 'Pik Moskau'}, {'name': 'Pik Moskau', 'height': 6785.0, 'coords': [38.948517, 71.834732], 'isolation': 15.7, 'next_high': 'Pik Ismoil Somoni'}, {'name': 'Pik Ismoil Somoni', 'height': 7495.0, 'coords': [38.9426, 72.0151], 'isolation': 278.0, 'next_high': 'Kongur Jiubie'},
     # {'name': 'Kongur Jiubie', 'height': 7530.0, 'coords': [38.615796, 75.195643], 'isolation': 10.52, 'next_high': 'Kongur'}, {'name': 'Kongur', 'height': 7649.0, 'coords': [38.5933, 75.31269], 'isolation': 240.0, 'next_high': 'Batura Sar'}, {'name': 'Batura Sar', 'height': 7795.0, 'coords': [36.510329, 74.522602], 'isolation': 62.98, 'next_high': 'Distaghil Sar'}, {'name': 'Distaghil Sar', 'height': 7885.0, 'coords': [36.325702, 75.188224], 'isolation': 129.27, 'next_high': 'K2'}, {'name': 'K2', 'height': 8611.0, 'coords': [35.88333333, 76.51666667], 'isolation': 1315.77, 'next_high': 'Mount Everest'}, {'name': 'Mount Everest', 'height': 8848.0, 'coords': [27.98823, 86.92501]}]
-    iso_list = wiki2.loop(input_text)
+    iso_list = wiki2.loop(input_mountain)
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(file_dir)
---
+++ wikiSearchScript.js
@@ -0,0 +1,49 @@
+var typingTimer; // Timer for input debounce
+
+function wikiSearchUpdate() {
+    var searchTerm = document.getElementById('wikiInput').value.trim();
+    if (!searchTerm) {
+        clearResults();
+        return;
+    }
+    
+    fetch(`/cgi-bin/opensearch_script.py?query=${encodeURIComponent(searchTerm)}`)
+        .then(response => response.json())
+        .then(data => {
+            displayResults(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            displayError('Error fetching data. Please try again later.');
+        });
+}
+
+function displayResults(data) {
+    var searchResults = data[1]; // Array of search results
+    var resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = ''; // Clear previous results
+    
+    if (searchResults.length === 0) {
+        resultsContainer.innerHTML = '<p>No results found.</p>';
+        return;
+    }
+
+    searchResults.forEach(result => {
+        var resultItem = document.createElement('a');
+        resultItem.classList.add('result-item');
+        resultItem.textContent = result;
+        resultItem.addEventListener('click', function() {
+            document.getElementById('wikiInput').value = result;
+            document.getElementById('wikiSearchForm').submit();
+        });
+        resultsContainer.appendChild(resultItem);
+    });
+}
+
+function clearResults() {
+    document.getElementById('results').innerHTML = '';
+}
+
+function displayError(message) {
+    document.getElementById('results').innerHTML = `<p>${message}</p>`;
+}
