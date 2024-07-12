125ce9f4dab0c9795ec9c1c2111e7011b15d4ad9
SimGo123 <43723284+SimGo123@users.noreply.github.com>
2024-07-12 14:45:24 +0200

    Switch to adjacent 'world' when crossing dateline

--- select-mountain/isolationMapping.js
+++ select-mountain/isolationMapping.js
@@ -58,7 +58,7 @@ function startIsolationLoop(mountain) {
 }
 
 function setupMap() {
-    osm = L.map('osm').setView([47.8, 11.2], 3);
+    osm = L.map('osm',{noWrap:true}).setView([47.8, 11.2], 3);
     // Für OSM: Erstelle Karte mit Urheberrecht und Tiles (Kacheln), aus denen die Karte zusammengesetzt ist
     const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contribiutors';
     const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
@@ -79,6 +79,7 @@ function cleanup() {
     markerList = [];
     lineList = [];
     circleList = [];
+    crossingDateline = false;
 }
 
 function drawIsoList(changeMapPos=false) {
@@ -88,6 +89,7 @@ function drawIsoList(changeMapPos=false) {
 
     const colors = ['red','blue','green','orange','violet', 'yellow'];
     for (let i = 0; i < iso_list.length; i++) {
+        let isStart = i==0;
         let fixedParams = iso_list[i][0];
         let name = fixedParams['name'];
         mountainListDiv.innerHTML += name + ' - ';
@@ -96,24 +98,43 @@ function drawIsoList(changeMapPos=false) {
         if (fixedParams.hasOwnProperty('isolation_dist')) {
             isolation_dist = fixedParams['isolation_dist'];
         }
-        let coords = fixedParams['coords'];
+
+        let coords = new L.LatLng(fixedParams['coords'][0], fixedParams['coords'][1]);
+        currPoint = new L.LatLng(coords.lat, coords.lng);
+        if (!isStart) {
+            // If line between prevPoint and currrPoint crosses date line,
+            // go across the [-180,180] longitude boundaries
+            // This means drawing on a 'world' adjacent to the current one
+            // All points after the dateline are drawn on the adjacent world,
+            // as the condition below will be fulfilled
+            // Warning: This only works when crossing the line just once
+            if (Math.abs(prevPoint.lng - currPoint.lng) >= 180) {
+                if (prevPoint.lng < 0) { // America -> Asia
+                    currPoint.lng = -180 - (180 - currPoint.lng);
+                } else { // Asia -> America
+                    currPoint.lng = 180 + (180 + currPoint.lng);
+                }
+            }
+        }
+
         if (changeMapPos) {
-            osm.setView([coords[0], coords[1]], 7);
+            osm.setView(coords, 7);
         }
 
         let colorToUse = colors[i % colors.length];
         var showMarker = document.getElementById("cbShowMarker").checked;
         var showCircle = document.getElementById("cbShowCircle").checked;
         if (showMarker) {
-            drawMarker(iso_list[i], name, coords, colorToUse);
+            drawMarker(iso_list[i], name, colorToUse, coords);
         }
 
         if (showCircle) {
-            drawCircle(isolation_dist, coords, colorToUse);
+            drawCircle(isolation_dist, colorToUse);
         }
 
-        let isStart = i==0;
-        drawLine(isStart, coords)
+        drawLine(isStart);
+        // Set the current point to become the later previous one ;)
+        prevPoint = currPoint;
     }
     drawHeightGraph(false);
 
@@ -122,7 +143,7 @@ function drawIsoList(changeMapPos=false) {
     }
 }
 
-function drawMarker(curr_iso, name, coords, colorToUse) {
+function drawMarker(curr_iso, name, colorToUse, coords) {
     var colorIcon = new L.Icon({
         iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${colorToUse}.png`,
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -131,11 +152,11 @@ function drawMarker(curr_iso, name, coords, colorToUse) {
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
     });
-    var marker = L.marker([coords[0], coords[1]], {icon: colorIcon}).addTo(osm);
+    var marker = L.marker(currPoint, {icon: colorIcon}).addTo(osm);
     markerList.push(marker);
     // Popup text for the marker
     infoboxComplete = curr_iso[1];
-    infoboxComplete['Koordinaten'] = `${coords[0].toFixed(5)},${coords[1].toFixed(5)}`;
+    infoboxComplete['Koordinaten'] = `${coords.lat.toFixed(5)},${coords.lng.toFixed(5)}`;
     let markerTxt = `<h3>${name} (<a href="https://de.wikipedia.org/wiki/${name}">Wiki</a>)</h3>`;
     for (const key in infoboxComplete) {
         if (infoboxComplete.hasOwnProperty(key)) {
@@ -149,16 +170,15 @@ function drawMarker(curr_iso, name, coords, colorToUse) {
     marker.bindPopup(markerTxt);
 }
 
-function drawCircle(isolation_dist, coords, colorToUse) {
-    var circle = L.circle([coords[0], coords[1]], {
+function drawCircle(isolation_dist, colorToUse) {
+    var circle = L.circle(currPoint, {
         radius: isolation_dist,
         color: colorToUse
     }).addTo(osm);
     circleList.push(circle);
 }
 
-function drawLine(isStart, coords) {
-    currPoint = new L.LatLng(coords[0], coords[1]);
+function drawLine(isStart) {
     // For a line, >= 2 points are needed -> don't draw on start
     if (isStart) {
         isStart = false;
@@ -178,8 +198,6 @@ function drawLine(isStart, coords) {
         lineList.push(line);
         line.addTo(osm);
     }
-    // Set the current point to become the later previous one ;)
-    prevPoint = currPoint;
 }
 
 function drawHeightGraph(redraw) {
